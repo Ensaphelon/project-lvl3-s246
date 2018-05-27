d1ecd77c809973906de196799597542207802410
Ramil Musaev <r.musaev@astrio.net>
2018-05-27 11:10:32 +0400

    update changed to setTimeout and uses Promise.all

--- src/rss.js
+++ src/rss.js
@@ -5,38 +5,39 @@ import renderArticles from './renderer';
 
 const sortArticlesByTime = articles => articles.sort((a, b) => a.date < b.date);
 
-const addNewFeed = (items, url, state) => {
-  state.feeds.push({ url, items });
-  renderArticles(items);
-};
-
 const updateFeed = (articles, feedUrl, state) => {
-  const existedFeed = find(state.feeds, feed => feed.url === feedUrl);
-  const diffCount = articles.length - existedFeed.items.length;
-  if (diffCount) {
-    const newArticles = articles.slice(0, diffCount);
-    newArticles.forEach(article => existedFeed.items.unshift(article));
-    renderArticles(newArticles);
+  const existedFeed = find(state.feeds, feed => feed.feedUrl === feedUrl);
+  if (existedFeed) {
+    const diffCount = articles.length - existedFeed.articles.length;
+    if (diffCount) {
+      const newArticles = articles.slice(0, diffCount);
+      newArticles.forEach(article => existedFeed.articles.unshift(article));
+      renderArticles(newArticles);
+    }
+  } else {
+    state.feeds.push({ feedUrl, articles });
+    renderArticles(articles);
   }
 };
 
-const loadRss = (url, action, state) => {
-  get('https://cors-proxy.htmldriven.com/', {
-    params: { url },
-  }).then((response) => {
-    action(sortArticlesByTime(parseRss(response.data.body)), url, state);
-  }).catch((error) => {
-    console.error(error.response.data.message);
-  });
-};
+const loadRss = (url, action, state) => get('https://cors-proxy.htmldriven.com/', {
+  params: { url },
+}).then((response) => {
+  action(sortArticlesByTime(parseRss(response.data.body)), url, state);
+}).catch((error) => {
+  console.error(error.response.data.message);
+});
+
+const checkForUpdate = state => Promise.all(state.feeds
+  .map(feed => loadRss(feed.feedUrl, updateFeed, state)))
+  .then(() => setTimeout(() => checkForUpdate(state), 5000))
+  .catch(() => setTimeout(() => checkForUpdate(state), 5000));
 
 export default (url, state) => {
   const newState = state;
-  loadRss(url, addNewFeed, state);
+  loadRss(url, updateFeed, newState);
   if (!newState.updateIsRunning) {
     newState.updateIsRunning = true;
-    setInterval(() => {
-      state.feeds.map(feed => loadRss(feed.url, updateFeed, state));
-    }, 5000);
+    checkForUpdate(newState);
   }
 };
